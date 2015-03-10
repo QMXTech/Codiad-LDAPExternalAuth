42202b872638b58c6de5f98974c618eaba3295cd
Korynkai <matt@qmxtech.com>
2015-03-09 22:05:17 -0500

    Fix Codiad user handling, add 'createuser' variable and add documentation to README.md in prep to add similar content to Codiad's wiki (and bring this into full-production).

--- README.md
+++ README.md
@@ -5,10 +5,7 @@ Written by Korynkai of QuantuMatriX Technologies.
 
 Author's notes:
 As this is more of a configuration / authentication drop-in, I see no
-reason to create a separate license file. This is really just for
-anybody looking for something like this, and I don't see why Codiad
-shouldn't have it as an option anyway as LDAP authentication is
-generally common among groups running such servers anyway.
+reason to create a separate license file.
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
@@ -28,3 +25,32 @@ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
 LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+## Installation
+
+* Download `ldap.php`.
+* Edit `ldap.php` in a text editor, changing configuration values as needed.
+* Save `ldap.php` somewhere on the server, preferably somewhere within the Codiad root (I created a special directory for External Authentication called `auth` on my setup) and ensure the server has permissions to at least read the file.
+* Edit Codiad's `config.php` in a text editor, uncommenting and/or adding the line `define("AUTH_PATH", "/path/to/ldap.php");`. Replace "/path/to" with the actual path. You may use the `BASE_PATH` directive if you saved `ldap.php` to somewhere within the Codiad root. For example, on my setup (with the `auth` directory), this is set to `define("AUTH_PATH", BASE_PATH . "/auth/ldap.php");`
+* (Optionally) back up your Codiad `data/users.php` file to somewhere safe (in case of an LDAP issue), then edit `data/users.php` so every field marked `"password"` looks like `"password":null`. This is probably unnecessary, but ensures that no Codiad internal passwords are used and authentication occurs strictly over LDAP.
+
+## Configuration
+
+Most of the configuration should be completed during the installation; However it would be wise to explain these values:
+
+* `$server` would be your LDAP server's connection URI; For example:
+ * `$server = 'ldap://ldap.example.com:389';`
+
+* `$basedn` would be your LDAP server's search base distinguished name. This would be where Codiad looks for user entries within LDAP. Example:
+ * `$basedn = 'ou=people,dc=example,dc=com';`
+
+* `$filter` is your LDAP user search filter. This tells Codiad which attribute/value pairs to look for as the username to look up. If you aren't sure what to do here, you may use one of the alternatives or use the references either at http://tools.ietf.org/search/rfc4515 (quite technical IETF RFC) or http://goo.gl/FOdGp7 (CentOS documentation page on LDAP search filters). The variable `$1` must always be supplied as a value as it signifies the username. The default will allow a CN or an email to log in; however, the user environments between the CN and email logins would differ, essentially acting as separate users within Codiad. Examples:
+ * `$filter = '(&(objectClass=*)(|(cn=$1)(email=$1)))';` <-- Allows CN or email to denote the username. As it uses a logical `or` (`|`), it allows more than one field to directly act as the username, in effect allowing each LDAP user (with both a CN and an email attribute) to create/log-in to two Codiad users if they so desire.
+ * `$filter = '(&(objectClass=*)(cn=$1))';` <-- Strictly use CN as username.
+ * `$filter = '(&(objectClass=*)(email=$1))';` <-- Strictly use email as username.
+ * `$filter = '(&(objectClass=*)(uniqueIdentifier=$1))';` <-- Strictly use uniqueIdentifier as username (This is useful for custom self-identifiable usernames; This is the filter we use on our setup, however it requires additional configuration on LDAP.
+
+* `$createuser` either allows or denies the automatic creation of a Codiad user upon successful LDAP authentication. If set to true, a `user` will be created if the user successfully authenticates through LDAP but is not present within Codiad's `data/users.php` file. If set to `false`, the user will be denied access if they are not present within Codiad's `data/users.php` file, regardless of whether or not the user has successfully authenticated to LDAP. Default is `true`.
+
+* `$version` -- **_DO NOT CHANGE THIS!!! I SERIOUSLY CANNOT STRESS THIS ENOUGH!!!_** The version parameter is the LDAP **_PROTOCOL_** version used by the server and should not be changed unless you are **_ABSOLUTELY, POSITIVELY, 100% CERTAIN_** you are using an oddball version of the **_PROTOCOL_**, either a custom variation, a newer version (highly unlikely -- the protocol versions are directly managed by IETF and are rare to come out -- v3 came out in 1997 and officially made v2 obsolete in 2003, to give an idea) or a version predating 2003. --**_DO NOT_** confuse this with the OpenLDAP, ActiveDirectory, 389, etc... _server_ version. This is **_not_** the same thing.
+
--- ldap.php
+++ ldap.php
@@ -6,10 +6,7 @@
 //									   //
 // Author's notes:							   //
 // As this is more of a configuration / authentication drop-in, I see no   //
-// reason to create a separate license file. This is really just for	   //
-// anybody looking for something like this, and I don't see why Codiad	   //
-// shouldn't have it as an option anyway as LDAP authentication is	   //
-// generally common among groups running such servers anyway.		   //
+// reason to create a separate license file.				   //
 //									   //
 // Permission is hereby granted, free of charge, to any person obtaining   //
 // a copy of this software and associated documentation files (the	   //
@@ -31,12 +28,9 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.	   //
 /////////////////////////////////////////////////////////////////////////////
 
-////////////////////////
-// Server information //
-////////////////////////
-
-// The LDAP version to use (should normally be '3')
-    $version = 3;
+///////////////////
+// Configuration //
+///////////////////
 
 // The LDAP connection URI of the server.
     $server = 'ldap://ldap.example.com:389';
@@ -44,51 +38,95 @@
 // The DN to search under on the server.
     $basedn = 'ou=people,dc=example,dc=com';
 
-// The search filter. If you aren't sure what this is, this seems to be a good
-// reference: 
-//   http://www.ldapexplorer.com/en/manual/109010000-ldap-filter-syntax.htm
+// The LDAP search filter. If you aren't sure what this is, the official
+// IETF RFC definition (quite technical) is here: 
+//	http://tools.ietf.org/search/rfc4515
+// and you can find another good (easier to follow) reference here
+// (shortened centos link):
+//	http://goo.gl/FOdGp7
+// The default will allow a CN or an email to log in (however, the user 
+// environments between the CN and email logins would differ).
 // Default is: '(&(objectClass=*)(|(cn=$1)(email=$1)))'.
+// A couple alternatives for simple set-ups: 
+//	CN only: '(&(objectClass=*)(cn=$1))'
+//	email only: '(&(objectClass=*)(email=$1))'
     $filter = '(&(objectClass=*)(|(cn=$1)(email=$1)))';
+
+// Optionally create Codiad user if it doesn't already exist. This can be set
+// to 'false' if the administrator would like to manually control access to 
+// Codiad from within Codiad itself, rather than let the search filter fully
+// dictate user access control. 
+// Default is 'true'.
+    $createuser = true;
+
+// The LDAP protocol version to use. Changing this is probably a very bad 
+// idea unless you're absolutely positive you are using some completely 
+// non-standard or obsolete (pre-2003) version of LDAP.
+// Default is '3'. Developer heavily discourages changing this.
+    $version = 3;
 
 /////////////////////////////////////////////////////////////////////////////
 // Do not edit anything under this line unless you know what you're doing! //
 /////////////////////////////////////////////////////////////////////////////
 
-    if ( !isset( $_SESSION['user'] ) ) {
+    require_once( COMPONENTS . "/user/class.user.php" );
 
-    	if( isset( $_POST['username'] ) && isset( $_POST['password'] ) ) {
+    if ( !isset( $_SESSION["user"] ) ) {
 
-	    $username = $_POST['username'];
-	    $password = $_POST['password'];
+        if ( isset( $_POST["username"] ) && isset( $_POST["password"] ) ) {
 
-	    $tfilter = str_replace( '$1', $username, $filter );
-	    $socket = ldap_connect( $server );
+            $User = new User();
 
-	    ldap_set_option( $socket, LDAP_OPT_PROTOCOL_VERSION, $version );
-	    ldap_set_option( $socket, LDAP_OPT_REFERRALS, 0 );
+            $User->username = $_POST["username"];
+            $User->password = $_POST["password"];
 
-   	    if ( $socket == true ) {
+            $tfilter = str_replace( "$1", $User->username, $filter );
+            $socket = ldap_connect( $server );
+
+            ldap_set_option( $socket, LDAP_OPT_PROTOCOL_VERSION, $version );
+            ldap_set_option( $socket, LDAP_OPT_REFERRALS, 0 );
+
+            if ( $socket == true ) {
 
                 $result = ldap_search( $socket, $basedn, $tfilter );
                 $count  = ldap_count_entries( $socket, $result );
 
                 if ( $count === 1 ) {
+
                     $data = ldap_get_entries( $socket, $result );
-                    $auth = ldap_bind( $socket, $data[0]["dn"], $password );
+                    $auth = ldap_bind( $socket, $data[0]["dn"], $User->password );
 
                     if ( $auth === -1 ) {
 
-                        die( formatJSEND( "error", "An LDAP error has occurred: " . ldap_error($socket)));
+                        die( formatJSEND( "error", "An LDAP error has occurred: " . ldap_error($socket) ) );
 
-                    } else if ( $auth == false ) {
+                    } elseif ( $auth == false ) {
 
-                        die( formatJSEND( "error", "Password does not match." . $pwattr ) );
+                        die( formatJSEND( "error", "Password does not match." ) );
 
-                    } else if ( $auth == true ) {
+                    } elseif ( $auth == true ) {
 
-                        $_SESSION["user"] = $username;
+                        if ( $User->CheckDuplicate() ) {
 
-                        if (isset($_POST["language"])) {
+                            if ( $createuser == true ) {
+
+                                $User->users[] = array( "username" => $User->username, "password" => null, "project" => "" );
+                                saveJSON( 'users.php', $User->users );
+                                $_SESSION["user"] = $User->username;
+
+                            } else {
+
+                                die( formatJSEND( "error", "User " . $User->username . " does not exist within Codiad." ) );
+
+                            }
+
+                        } else {
+
+                            $_SESSION["user"] = $User->username;
+
+                        }
+
+                        if ( isset( $_POST["language"] ) ) {
 
                             $_SESSION["lang"] = $_POST["language"];
 
@@ -99,18 +137,12 @@
                         }
 
                         $_SESSION["theme"] = $_POST["theme"];
+                        $_SESSION["project"] = $_POST["project"];
 
-                        if (isset($_POST["project"])) {
+                        echo formatJSEND( "success", array( "username" => $User->username ) );
+                        header( "Location: " . $_SERVER["PHP_SELF"] . "?action=verify" );
 
-                            $_SESSION["project"] = $_POST["project"];
-
-                        }
-
-                        echo formatJSEND("success", array("username"=>$_SESSION["user"]));
-
-                        header("Location: ".$_SERVER["PHP_SELF"]."?action=verify");
-
-                        }
+                    }
 
                 } elseif ( $count > 1 ) {
 
@@ -119,19 +151,17 @@
 
                 } else {
 
-                    die( formatJSEND( "error", "LDAP user $username does not exist." ) );
+                    die( formatJSEND( "error", "LDAP user " . $User->username . " does not exist." ) );
 
                 }
 
-	    } else {
+            } else {
 
-	    	die( formatJSEND( "error", "An error occurred: Cannot connect to LDAP server. Please contact the webmaster.\n
-					        If you are the webmaster, please contact your LDAP server administrator or check if your LDAP server is running." ) );
+                die( formatJSEND( "error", "An error occurred: Cannot connect to LDAP server. Please contact the webmaster. 
+                                        If you are the webmaster, please contact your LDAP server administrator or check if your LDAP server is running." ) );
 
-	    }
-
-        }
-
+            }
+	}
     }
 
 ?>
